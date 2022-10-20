8f80556da30d86de29d75fe70535136bffdefc43
hanfei8987 <hanfei8987@gmail.com>
2022-10-20 19:38:34 +0800

    first commit

---
+++ ts-client/planet.blog/types.ts
@@ -0,0 +1,21 @@
+import { BlogPacketData } from "./types/blog/packet"
+import { NoData } from "./types/blog/packet"
+import { IbcPostPacketData } from "./types/blog/packet"
+import { IbcPostPacketAck } from "./types/blog/packet"
+import { Params } from "./types/blog/params"
+import { Post } from "./types/blog/post"
+import { SentPost } from "./types/blog/sent_post"
+import { TimedoutPost } from "./types/blog/timedout_post"
+
+
+export {     
+    BlogPacketData,
+    NoData,
+    IbcPostPacketData,
+    IbcPostPacketAck,
+    Params,
+    Post,
+    SentPost,
+    TimedoutPost,
+    
+ }
